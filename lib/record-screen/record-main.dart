06c2c9c1ead9eedacf3fbc4b78465c5fa874d8bf
SwiftfoxStudios <jlwdevs@gmail.com>
2020-11-16 15:13:39 +0000

    update; added anims

--- lib/record-screen/record-main.dart
+++ lib/record-screen/record-main.dart
@@ -1,11 +1,14 @@
 // MAIN DART PARENT IMPORT
 import 'dart:async';
+import 'dart:math';
 
 // FILE DEPENDENCIES
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart';
 import 'package:location/location.dart';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 // COLOURS USED IN PROJECT
 const bgDark = 0xff202020;
@@ -21,8 +24,8 @@ class RecordMain extends StatefulWidget {
 // EXTENSION OF INSTANTIATION
 class _RecordMainState extends State<RecordMain> {
   Completer<GoogleMapController> _controller = Completer();
-  @override
-  Location location = new Location();
+
+  Location location = Location();
   int _selectedIndex = 1;
   bool isSwitched = false;
 
@@ -120,7 +123,6 @@ class _RecordMainState extends State<RecordMain> {
                         onChanged: (value) {
                           setState(() {
                             isSwitched = value;
-                            print(isSwitched);
                           });
                         },
                         inactiveTrackColor: Colors.white,
@@ -153,7 +155,7 @@ class _RecordMainState extends State<RecordMain> {
 }
 
 class Recording extends StatefulWidget {
-  bool travelType;
+  final bool travelType;
   Recording({this.travelType});
 
   @override
@@ -161,11 +163,32 @@ class Recording extends StatefulWidget {
 }
 
 class _RecordingState extends State<Recording> {
+  Completer<GoogleMapController> _controller = Completer();
+  StreamSubscription locationSubscription;
+  Location location = Location();
   bool travelType;
   Color testVal;
+  FlutterLocalNotificationsPlugin notifPlugin;
+
+  var clock = Stopwatch();
+  final duration = const Duration(seconds: 1);
+  String displayTime = "0:00";
+  String displayDistance = "0.0";
+  bool clockIsPaused = true;
+  Icon isPaused;
+  bool started = false;
+  double cachedLat;
+  double cachedLon;
+  double journeyDistance = 0;
+  double cachedDistance = 0;
+
+  final Set<Polyline> polyline = {};
+  List<LatLng> visitedPoints = [];
+  bool selected = false;
+
+  static final CameraPosition initial = CameraPosition(target: LatLng(51.453318, -0.102559), zoom: 16);
   _RecordingState(this.travelType);
 
-  @override
   void parseType() {
     if (travelType) {
       testVal = Colors.green;
@@ -174,8 +197,246 @@ class _RecordingState extends State<Recording> {
     }
   }
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    locationSubscription.cancel();
+    super.dispose();
+  }
+
+  Future<void> moveCamera() async {
+    var pos = await location.getLocation();
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(pos.latitude, pos.longitude), zoom: 15)));
+  }
+
+  void checkPaused() {
+    if (clockIsPaused) {
+      isPaused = Icon(
+        Icons.play_arrow,
+        color: Colors.white,
+        size: 35.0,
+      );
+    } else {
+      isPaused = Icon(
+        Icons.pause,
+        color: Colors.white,
+        size: 35.0,
+      );
+    }
+  }
+
+  void startClock() {
+    clockIsPaused = false;
+    Timer(duration, iterateClock);
+  }
+
+  void pauseClock() {
+    clockIsPaused = true;
+  }
+
+  void iterateClock() {
+    if (clock.isRunning) {
+      startClock();
+    }
+    int elapsedHours = clock.elapsed.inHours;
+    int elapsedMinutes = clock.elapsed.inMinutes % 60;
+    int elapsedSeconds = clock.elapsed.inSeconds % 60;
+    setState(() {
+      if (elapsedHours == 0) {
+        displayTime = elapsedMinutes.toString() + ":" + elapsedSeconds.toString().padLeft(2, "0");
+      } else {
+        displayTime = elapsedHours.toString() + ":" + elapsedMinutes.toString().padLeft(2, "0") + ":" + elapsedSeconds.toString().padLeft(2, "0");
+      }
+    });
+  }
+
+  void startListening() async {
+    location.changeSettings(distanceFilter: 5, accuracy: LocationAccuracy.high);
+    locationSubscription = location.onLocationChanged.listen((LocationData positionUpdate) {
+      updatePosition(positionUpdate.latitude, positionUpdate.longitude);
+    });
+  }
+
+  void stopListening() {
+    print("hello");
+    locationSubscription.cancel();
+  }
+
+  updatePosition(double finalLat, double finalLon) async {
+    print(visitedPoints);
+    double initialLat;
+    double initialLon;
+    if (!started) {
+      var initialPosition = await location.getLocation();
+      initialLat = initialPosition.latitude;
+      initialLon = initialPosition.longitude;
+      setState(() {
+        visitedPoints.add(LatLng(initialPosition.latitude, initialPosition.longitude));
+      });
+      started = true;
+    } else {
+      initialLat = cachedLat;
+      initialLon = cachedLon;
+      setState(() {
+        visitedPoints.add(LatLng(cachedLat, cachedLon));
+      });
+    }
+    cachedDistance = calculateDistance(initialLat, initialLon, finalLat, finalLon);
+    journeyDistance += cachedDistance;
+    print(journeyDistance.round());
+    setState(() {
+      displayDistance = (journeyDistance.round() / 1000).toStringAsFixed(2);
+    });
+  }
+
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    cachedLat = lat2;
+    cachedLon = lon2;
+    // HAVERSINE FORMULA
+    const double earthRadius = 6371e3;
+    double lat1rad = lat1 * pi / 180;
+    double lat2rad = lat2 * pi / 180;
+
+    double deltaLat = (lat2 - lat1) * pi / 180;
+    double deltaLon = (lon2 - lon1) * pi / 180;
+
+    double angle = sin(deltaLat / 2) * sin(deltaLat / 2) + cos(lat1rad) * cos(lat2rad) * sin(deltaLon / 2) * sin(deltaLon / 2);
+
+    double angularDistance = 2 * atan2(sqrt(angle), sqrt(1 - angle));
+    double returnedDistance = earthRadius * angularDistance;
+    return returnedDistance;
+    // Returned value is in metres
+  }
+
+  Future<bool> showReturnError() {
+    /*
+        DESC: Catches user pressing back button
+        PARAMS: null
+        RETURNS: Error dialog box
+    */
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Are you sure you want to exit?"),
+          backgroundColor: Colors.white,
+          content: new Text("This will cancel your route and all progress you've made."),
+          actions: <Widget>[
+            FlatButton(
+              child: new Text("NO"),
+              color: Color(accent),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            FlatButton(
+              child: new Text("YES, EXIT"),
+              color: Colors.green,
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void addPoly() {
+    PolylineId id = PolylineId("test");
+    setState(() {
+      polyline.add(Polyline(polylineId: id, points: visitedPoints, width: 5, color: Color(accent)));
+    });
+  }
+
   Widget build(BuildContext context) {
     parseType();
-    return Scaffold(backgroundColor: testVal);
+    checkPaused();
+    moveCamera();
+    addPoly();
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final statusHeight = MediaQuery.of(context).padding.top;
+    return WillPopScope(
+        onWillPop: showReturnError,
+        child: Scaffold(
+          // PARENT APP BAR
+          // PAGE BODY
+          body: Column(children: <Widget>[
+            Container(height: statusHeight),
+            AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                // GOOGLE MAP CONTAINER
+                height: selected ? 200 : deviceHeight / 1.4,
+                child: GoogleMap(
+                    initialCameraPosition: initial,
+                    zoomControlsEnabled: false,
+                    myLocationEnabled: true,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                    polylines: polyline)),
+            Stack(children: <Widget>[
+              AnimatedContainer(
+                // BREAKPOINT FOR SOME BULLSHIT I CANT FUCKING REMEMBER
+                // too bad !
+                duration: Duration(milliseconds: 200),
+                height: !selected ? deviceHeight - deviceHeight / 1.4 - statusHeight : deviceHeight - statusHeight - 200,
+                decoration: new BoxDecoration(color: Color(bgDark)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [Text(displayDistance, style: TextStyle(fontSize: deviceHeight / 16, color: Colors.white)), Text(displayTime, style: TextStyle(fontSize: deviceHeight / 16, color: Colors.white))]),
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [Text("KM", style: TextStyle(fontSize: deviceHeight / 30, color: Colors.white)), Text("TIME", style: TextStyle(fontSize: deviceHeight / 30, color: Colors.white))]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RawMaterialButton(
+                          onPressed: () {
+                            setState(() {
+                              selected = !selected;
+                            });
+                            if (clockIsPaused) {
+                              startListening();
+
+                              clock.start();
+                              startClock();
+                            } else {
+                              stopListening();
+                              clock.stop();
+                              pauseClock();
+                            }
+                          },
+                          elevation: 2.0,
+                          fillColor: Color(accent),
+                          child: isPaused,
+                          padding: EdgeInsets.all(15.0),
+                          shape: CircleBorder(),
+                        ),
+                        RawMaterialButton(
+                          onPressed: () {},
+                          elevation: 2.0,
+                          fillColor: Colors.white,
+                          child: Icon(
+                            Icons.fullscreen,
+                            color: Colors.black,
+                            size: 35.0,
+                          ),
+                          padding: EdgeInsets.all(15.0),
+                          shape: CircleBorder(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ]),
+          ]),
+        ));
   }
 }
